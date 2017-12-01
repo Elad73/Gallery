fcbcf8a4a09e3e2625571a87cb8de32928e5b2c7
Elad73 <elad.ron.g@gmail.com>
2017-12-01 16:00:17 +0200

    Removing the tineMCA WYSIWYG

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,10 +7,11 @@
       <!-- Bootstrap Core JavaScript -->
       <script src="js/bootstrap.min.js"></script>
 
-      <!-- WYSIWYG -->
+      <!-- WYSIWYG FROM tinyMCA - decided not to use it, if wants to uncomment here and on scripts.js-->
+      <!--
       <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-
       <script src="js/scripts.js"></script>
+      -->
 
   </body>
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,4 +1,4 @@
 /**
  * Created by Elad on 01/12/2017.
  */
-tinymce.init({ selector:'textarea' });
+/* tinymce.init({ selector:'textarea' });*/
