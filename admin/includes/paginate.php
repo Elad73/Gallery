7fbbc3beb911c17c0febfd7c35116e32b6b4a3c7
Elad73 <elad.ron.g@gmail.com>
2017-12-17 07:48:59 +0200

    Creating the Paginate class

---
+++ admin/includes/paginate.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elad
+ * Date: 17/12/2017
+ * Time: 07:41
+ */
+
+class Paginate {
+
+    protected $current_page;
+    protected $items_per_page;
+    protected $items_total_count;
+
+    public function __construct($page=1, $item_per_page=10, $items_total_count=0){
+
+        $this->setCurrentPage((int)$page);
+        $this->setItemsPerPage((int)$item_per_page);
+        $this->setItemsTotalCount((int)$items_total_count);
+    }
+
+    //region Getters & Setters
+    /**
+     * @return mixed
+     */
+    public function getCurrentPage()
+    {
+        return $this->current_page;
+    }
+
+    /**
+     * @param mixed $current_page
+     */
+    public function setCurrentPage($current_page)
+    {
+        $this->current_page = $current_page;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getItemsPerPage()
+    {
+        return $this->items_per_page;
+    }
+
+    /**
+     * @param mixed $items_per_page
+     */
+    public function setItemsPerPage($items_per_page)
+    {
+        $this->items_per_page = $items_per_page;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getItemsTotalCount()
+    {
+        return $this->items_total_count;
+    }
+
+    /**
+     * @param mixed $items_total_count
+     */
+    public function setItemsTotalCount($items_total_count)
+    {
+        $this->items_total_count = $items_total_count;
+    }
+    //endregion
+
+
+
+
+
+
+}
