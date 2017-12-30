28fe82023c8d5608a355f7b4ba955e9a23f4a20d
Elad73 <elad.ron.g@gmail.com>
2017-12-30 09:59:58 +0200

    Adding a blank "about me" page

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php include("includes/header.php"); ?>
+
+<div class="row">
+
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
+
+
+
+
+    </div>
+
+
+    <!-- Blog Sidebar Widgets Column -->
+    <!-- <div class="col-md-4"> -->
+    <?php // include("includes/sidebar.php"); ?>
+
+</div>
+<!-- /.row -->
+
+<?php include("includes/footer.php"); ?>
